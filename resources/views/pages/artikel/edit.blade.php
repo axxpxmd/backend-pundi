0f2fd60ecdd64089429732abdd1e693580c73d12
AsipHamdi <asiphamdi13@gmail.com>
2020-07-21 20:04:51 +0700

    update

--- resources/views/pages/artikel/edit.blade.php
+++ resources/views/pages/artikel/edit.blade.php
@@ -47,13 +47,13 @@
                         <div class="form-group row">
                             <label class="col-sm-3 col-form-label">Nama</label>
                             <div class="col-sm-9">
-                              <label class="form-control-plaintext -ml-30">:&nbsp; {{ $artikel->editor->name }}</label>
+                                <label class="form-control-plaintext -ml-30">:&nbsp; {{ $artikel->editor == null ? '-' : $artikel->editor->name }}</label>
                             </div>
                         </div>
                         <div class="form-group row -mt-20">
                             <label class="col-sm-3 col-form-label">Email</label>
                             <div class="col-sm-9">
-                              <label class="form-control-plaintext -ml-30">:&nbsp; {{ $artikel->editor->email }}</label>
+                                <label class="form-control-plaintext -ml-30">:&nbsp; {{ $artikel->editor == null ? '-' : $artikel->editor->email }}</label>
                             </div>
                         </div>
                     </div>
